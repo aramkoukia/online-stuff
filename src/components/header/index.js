77b821bc6f9ba31d184991421ad486ed7c41e3e5
aramkoukia <aramkoukia@gmail.com>
2018-11-26 11:30:40 -0800

    cleaned up header component

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,10 +1,6 @@
 import { h, Component } from 'preact';
 import { route } from 'preact-router';
 import TopAppBar from 'preact-material-components/TopAppBar';
-import Drawer from 'preact-material-components/Drawer';
-import List from 'preact-material-components/List';
-import Dialog from 'preact-material-components/Dialog';
-import Switch from 'preact-material-components/Switch';
 import 'preact-material-components/Switch/style.css';
 import 'preact-material-components/Dialog/style.css';
 import 'preact-material-components/Drawer/style.css';
@@ -13,19 +9,6 @@ import 'preact-material-components/TopAppBar/style.css';
 // import style from './style';
 
 export default class Header extends Component {
-	closeDrawer() {
-		this.drawer.MDComponent.open = false;
-		this.state = {
-			darkThemeEnabled: false
-		};
-	}
-
-	openDrawer = () => (this.drawer.MDComponent.open = true);
-
-	openSettings = () => this.dialog.MDComponent.show();
-
-	drawerRef = drawer => (this.drawer = drawer);
-	dialogRef = dialog => (this.dialog = dialog);
 
 	linkTo = path => () => {
 		route(path);
@@ -33,64 +16,30 @@ export default class Header extends Component {
 	};
 
 	goHome = this.linkTo('/');
-	goToMyProfile = this.linkTo('/profile');
-
-	toggleDarkTheme = () => {
-		this.setState(
-			{
-				darkThemeEnabled: !this.state.darkThemeEnabled
-			},
-			() => {
-				if (this.state.darkThemeEnabled) {
-					document.body.classList.add('mdc-theme--dark');
-				}
-				else {
-					document.body.classList.remove('mdc-theme--dark');
-				}
-			}
-		);
-	}
+	goSignIn = this.linkTo('/SignIn');
+	goSignUp = this.linkTo('/SignUp');
 
 	render(props) {
 		console.log(props.selectedRoute);
+		const pointerCursor = {cursor: 'pointer'};
 		return (
 			<div>
 				<TopAppBar className="topappbar">
 					<TopAppBar.Row>
 						<TopAppBar.Section align-start>
-							<TopAppBar.Icon menu onClick={this.openDrawer}>
+							<TopAppBar.Icon style={ pointerCursor } menu onClick={this.goHome}>
 								menu
 							</TopAppBar.Icon>
-							<TopAppBar.Title>Preact app</TopAppBar.Title>
-						</TopAppBar.Section>
-						<TopAppBar.Section align-end shrink-to-fit onClick={this.openSettings}>
-							<TopAppBar.Icon>settings</TopAppBar.Icon>
+							<TopAppBar.Title style={ pointerCursor } menu onClick={this.goHome}>
+								Rate My Pot
+							</TopAppBar.Title>
+						</TopAppBar.Section>															
+						<TopAppBar.Section align-end shrink-to-fit>
+							<TopAppBar.Title style={ pointerCursor } menu onClick={this.goSignIn}>Sign In</TopAppBar.Title>
+							<TopAppBar.Title style={ pointerCursor } menu onClick={this.goSignUp}>Join</TopAppBar.Title>
 						</TopAppBar.Section>
 					</TopAppBar.Row>
 				</TopAppBar>
-				<Drawer modal ref={this.drawerRef}>
-					<Drawer.DrawerContent>
-						<Drawer.DrawerItem selected={props.selectedRoute === '/'} onClick={this.goHome}>
-							<List.ItemGraphic>home</List.ItemGraphic>
-							Home
-						</Drawer.DrawerItem>
-						<Drawer.DrawerItem selected={props.selectedRoute === '/profile'} onClick={this.goToMyProfile}>
-							<List.ItemGraphic>account_circle</List.ItemGraphic>
-							Profile
-						</Drawer.DrawerItem>
-					</Drawer.DrawerContent>
-				</Drawer>
-				<Dialog ref={this.dialogRef}>
-					<Dialog.Header>Settings</Dialog.Header>
-					<Dialog.Body>
-						<div>
-							Enable dark theme <Switch onClick={this.toggleDarkTheme} />
-						</div>
-					</Dialog.Body>
-					<Dialog.Footer>
-						<Dialog.FooterButton accept>OK</Dialog.FooterButton>
-					</Dialog.Footer>
-				</Dialog>
 			</div>
 		);
 	}
