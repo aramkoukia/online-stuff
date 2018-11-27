ca648884fb41bf25cf12b1a4a56fffcbe910a11b
aramkoukia <aramkoukia@gmail.com>
2018-11-26 19:06:22 -0800

    making api call to blob storage

--- src/components/pot-list/pot-list.js
+++ src/components/pot-list/pot-list.js
@@ -5,12 +5,52 @@ import 'preact-material-components/Button/style.css';
 import Pot from '../pot/pot';
 
 export default class PotList extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			error: null,
+			isLoaded: false,
+			items: []
+		};
+	}
+
+	componentDidMount() {
+		fetch("https://qualitymj.blob.core.windows.net/main/products.json")
+		  .then(res => res.json())
+		  .then(
+			(result) => {
+			  this.setState({
+				isLoaded: true,
+				items: result
+			  });
+			},
+			// Note: it's important to handle errors here
+			// instead of a catch() block so that we don't swallow
+			// exceptions from actual bugs in components.
+			(error) => {
+			  this.setState({
+				isLoaded: true,
+				error
+			  });
+			}
+		  )
+	  }
+
 	render() {
-		return (
-			<div>
-				<Pot />
-				<Pot />
-			</div>
-		);
+		const { error, isLoaded, items } = this.state;
+		if (error) {
+			return <div>Error: {error.message}</div>;
+		} else if (!isLoaded) {
+		return <div>Loading...</div>;
+		} else {
+			return (
+				<div>
+				{items.map(item => (					
+					<Pot item={item} />
+				))}
+				</div>
+			);
+		}
 	}
 }
--- src/components/pot/pot.js
+++ src/components/pot/pot.js
@@ -6,15 +6,17 @@ import Icon from 'preact-material-components/Icon';
 import style from './style';
 
 export default class Pot extends Component {
+	
 	render() {
 		return (
 			<Card>
 				<div class={style.cardHeader}>
-					<h2 class=" mdc-typography--title">Pot 2</h2>
-					<div class=" mdc-typography--caption">Welcome to home route</div>
+					<h2 class=" mdc-typography--title">{this.props.item.title}</h2>
+					<div class=" mdc-typography--caption">{this.props.item.vendor}</div>
+					<div class=" mdc-typography--caption">{this.props.item.price}</div>
 				</div>
 				<div class={style.cardBody}>
-					Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+					<img src={this.props.item.image}></img>
 				</div>
 				<Card.Actions>
 
